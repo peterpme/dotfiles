38fb0567a8ad51c099a5950f799ddd54b68882e1
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2016-11-09 10:26:28 -0600

    add gitignore, macos settings, brew settings, setup

---
+++ brew.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+brew cask install virtualbox
+brew install docker docker-machine
---
+++ setup.sh
@@ -0,0 +1,2 @@
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
+

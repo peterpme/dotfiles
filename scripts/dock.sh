b9fa8d8dee54bfe4913a1f2563b7e2261bf81094
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2016-12-02 19:39:25 -0600

    script working wrap in main

--- scripts/dock.sh
+++ scripts/dock.sh
@@ -1,26 +1,39 @@
+#1/bin/bash
+
+# Remove Dock items
 echo -b 'Setting defaults for Dock...'
 
-if type dockutil &>/dev/null; then
+main() {
+  if type dockutil &>/dev/null; then
 
-    dockutil --no-restart \
-        --remove 'System Preferences' \
-        --remove 'App Store' \
-        --remove 'Maps' \
-        --remove 'Notes' \
-        --remove 'Photos' \
-        --remove 'Messages' \
-        --remove 'Contacts' \
-        --remove 'Calendar' \
-        --remove 'Reminders'\
-        --remove 'FaceTime' \
-        --remove 'Feedback Assistant' \
-        --remove 'Siri' \
-        --remove 'Launchpad' \
-        --remove 'Numbers' \
-        --remove 'Pages' \
-        --remove 'Keynote' \
-        --remove 'iTunes' \
-        --remove 'iBooks' \
-        &>/dev/null
+      dockutil --no-restart \
+          --remove 'System Preferences' \
+          --remove 'App Store' \
+          --remove 'Maps' \
+          --remove 'Notes' \
+          --remove 'Photos' \
+          --remove 'Messages' \
+          --remove 'Contacts' \
+          --remove 'Calendar' \
+          --remove 'Reminders'\
+          --remove 'FaceTime' \
+          --remove 'Feedback Assistant' \
+          --remove 'Siri' \
+          --remove 'Launchpad' \
+          --remove 'Numbers' \
+          --remove 'Pages' \
+          --remove 'Keynote' \
+          --remove 'iTunes' \
+          --remove 'iBooks' \
+          --remove 'Mail' \
+          &>/dev/null
 
-fi
+      killall Dock
+
+  else
+    echo 'ERROR: dockutil not found'
+  fi
+}
+
+main
+

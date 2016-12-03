2dfaf06218041972a45fc64730f5c512f3bee6f7
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2016-12-02 19:41:11 -0600

    move brew and brew cask

---
+++ scripts/brew_cask_install.sh
@@ -0,0 +1,35 @@
+brew update
+brew install caskroom/cask/brew-cask
+brew tap caskroom/versions
+
+# daily
+brew cask install dropbox
+# brew cask install gyazo
+brew cask install 1password
+brew cask install flux
+brew cask install alfred
+brew cask install hammerspoon
+brew cask install spotify
+
+# dev
+brew cask install iterm2
+brew cask install atom
+brew cask install imagealpha
+brew cask install imageoptim
+brew cask install sketch
+brew cask install google-cloud-sdk
+
+# browsers
+brew cask install google-chrome
+brew cask install google-chrome-canary
+brew cask install firefoxnightly
+
+# one-off
+brew cask install vlc
+brew cask install licecap
+brew cask install virtualbox
+brew cask install slack
+
+# Fonts!
+brew tap caskroom/fonts
+brew cask install font-fira-code

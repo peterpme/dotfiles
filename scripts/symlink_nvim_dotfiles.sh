88f8380e0ce90e3229060353363d686f1fd28d9f
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2019-10-03 13:32:24 -0500

    update nvim config

--- scripts/symlink_nvim_dotfiles.sh
+++ scripts/symlink_nvim_dotfiles.sh
@@ -1,2 +1,30 @@
-mkdir -p ~/.config
-ln -s ~/dotfiles/config/nvim ~/.config/nvim
+#!/usr/bin/env bash
+
+# mkdir -p ~/.config
+# ln -s ~/dotfiles/config/nvim ~/.config/nvim
+
+
+DOTFILES=$HOME/.dotfiles
+
+# create vim symlinks
+# As I have moved off of vim as my full time editor in favor of neovim,
+# I feel it doesn't make sense to leave my vimrc intact in the dotfiles repo
+# as it is not really being actively maintained. However, I would still
+# like to configure vim, so lets symlink ~/.vimrc and ~/.vim over to their
+# neovim equivalent.
+
+echo -e "\\n\\nCreating vim symlinks"
+echo "=============================="
+VIMFILES=( "$HOME/.vim:$DOTFILES/config/nvim"
+        "$HOME/.vimrc:$DOTFILES/config/nvim/init.vim" )
+
+for file in "${VIMFILES[@]}"; do
+    KEY=${file%%:*}
+    VALUE=${file#*:}
+    if [ -e "${KEY}" ]; then
+        echo "${KEY} already exists... skipping."
+    else
+        echo "Creating symlink for $KEY"
+        ln -s "${VALUE}" "${KEY}"
+    fi
+done

727aeda544890d560e2f1a08502f84296f3443b8
Peter Piekarczyk <peter@Peters-iMac.local>
2021-12-08 19:27:41 -0600

    light updates

--- hammerspoon/lights.lua
+++ hammerspoon/lights.lua
@@ -1,38 +1,45 @@
-local alert = require "hs.alert"
 local path = "http://homeassistant.local:8123"
 local turn_on = "/api/services/hue/hue_activate_scene"
 local turn_off = "/api/services/light/turn_off"
 
+local token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhNjU3OTNhZmM3Njg0YTRkYTJlY2Y2ZDU4ODZlMDI4YiIsImlhdCI6MTYzOTAwMjUwOSwiZXhwIjoxOTU0MzYyNTA5fQ.7SZKK8FFTaczdnHRm1s4qQoRYbC1bqEca8jNfE8BJpI" -- #gitignore
 
 local headers = {
    ["Authorization"] = "Bearer " .. token,
    ["Content-Type"] = "application/json"
 }
 
-local day_payload = [[ "group_name":"Desk lights", "scene_name":"Default" ]]
-local night_payload = [[ "group_name":"Desk lights", "scene_name":"Gamer" ]]
-
 sessionWatcher = nil
+
+local turn_on_path = path .. turn_on
+local turn_off_path = path .. turn_off
+
+local turn_on_payload = [[ {"group_name":"Desk lights", "scene_name":"Gamer"} ]]
+local turn_off_payload = [[ {"entity_id":"light.computer_backlight"} ]]
 
 function sessionChanged(eventType)
    hour = tonumber(os.date("%H"))
-
-   -- After 8pm before 6am
-   if (hour > 20 or hour < 3) then
-    if (eventType == hs.caffeinate.watcher.screensDidUnlock) then
-       hs.http.post(path .. turn_on, night_payload, headers)
-    end
-
-    if (eventType == hs.caffeinate.watcher.screensDidWake) then
-       hs.http.post(path .. turn_on, night_payload, headers)
-    end
-
-    if (eventType == hs.caffeinate.watcher.systemDidWake) then
-       hs.http.post(path .. turn_on, night_payload, headers)
-    end
-
+   -- After 6pm before 7am
+   if (hour > 18 or hour < 7) then
+      if (eventType == hs.caffeinate.watcher.screensDidUnlock) then
+         hs.http.post(turn_on_path, turn_on_payload, headers)
+      end
    else
-        hs.http.post(path .. turn_on, day_payload, headers)
+      if (eventType == hs.caffeinate.watcher.screensDidUnlock) then
+         hs.http.post(turn_on_path, turn_on_payload, headers)
+      end
+   end
+
+   if (eventType == hs.caffeinate.watcher.screensDidLock) then
+      hs.http.post(turn_off_path, turn_off_payload, headers)
+   end
+
+   if (eventType == hs.caffeinate.watcher.screensDidSleep) then
+      hs.http.post(turn_off_path, turn_off_payload, headers)
+   end
+
+   if (eventType == hs.caffeinate.watcher.screensDidPowerOff) then
+      hs.http.post(turn_off_path, turn_off_payload, headers)
    end
 end
 

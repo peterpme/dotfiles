02a21f7d28a3586d7ff998ec152f728ea2f66ad1
Peter Piekarczyk <peterpme@users.noreply.github.com>
2021-12-18 04:22:35 +0000

    updates homebrew

--- first_time_setup.sh
+++ first_time_setup.sh
@@ -9,7 +9,7 @@ sudo spctl --master-disable
 # Install Homebrew
 if test ! "$( command -v brew )"; then
     echo "Installing homebrew"
-    ruby -e "$( curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install )"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 echo 'Install Homebrew dependencies'

b92e9d8961ae2fd3fc1f81c495c11979413e5214
Peter Piekarczyk <peterpme@users.noreply.github.com>
2021-12-18 17:03:34 +0000

    updates xcode script

--- scripts/xcode.sh
+++ scripts/xcode.sh
@@ -1,45 +1,67 @@
 #!/bin/bash
 
-# Install Xcode Developer Tools
-# https://github.com/alrra/dotfiles/blob/ff123ca9b9b/os/os_x/installs/install_xcode.sh
+# https://github.com/alrra/dotfiles/blob/main/src/os/utils.sh
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "./utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+agree_with_xcode_licence() {
+
+    # Automatically agree to the terms of the `Xcode` license.
+    # https://github.com/alrra/dotfiles/issues/10
+
+    sudo xcodebuild -license accept &> /dev/null
+    print_result $? "Agree to the terms of the Xcode licence"
+
+}
+
+are_xcode_command_line_tools_installed() {
+    xcode-select --print-path &> /dev/null
+}
+
+install_xcode_command_line_tools() {
+
+    # If necessary, prompt user to install
+    # the `Xcode Command Line Tools`.
+
+    xcode-select --install &> /dev/null
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Wait until the `Xcode Command Line Tools` are installed.
+
+    execute \
+        "until are_xcode_command_line_tools_installed; do \
+            sleep 5; \
+         done" \
+        "Xcode Command Line Tools"
+
+}
+
+is_xcode_installed() {
+    [ -d "/Applications/Xcode.app" ]
+}
+
+set_xcode_developer_directory() {
+
+    # Point the `xcode-select` developer directory to
+    # the appropriate directory from within `Xcode.app`.
+    #
+    # https://github.com/alrra/dotfiles/issues/13
+
+    sudo xcode-select -switch "/Applications/Xcode.app/Contents/Developer" &> /dev/null
+    print_result $? "Make 'xcode-select' developer directory point to the appropriate directory from within Xcode.app"
+
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 main() {
-
-    if ! xcode-select --print-path &> /dev/null; then
-
-        # Prompt user to install the XCode Command Line Tools
-        xcode-select --install &> /dev/null
-
-        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-        # Wait until the XCode Command Line Tools are installed
-        until xcode-select --print-path &> /dev/null; do
-            sleep 5
-        done
-
-        # print_result $? 'Install XCode Command Line Tools'
-
-        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-        # Point the `xcode-select` developer directory to
-        # the appropriate directory from within `Xcode.app`
-        # https://github.com/alrra/dotfiles/issues/13
-
-        sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer
-        # print_result $? 'Make "xcode-select" developer directory point to Xcode'
-
-        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-        # Prompt user to agree to the terms of the Xcode license
-        # https://github.com/alrra/dotfiles/issues/10
-
-        sudo xcodebuild -license
-        # print_result $? 'Agree with the XCode Command Line Tools licence'
-
-    fi
-
-    # print_result $? 'XCode Command Line Tools'
-
+    install_xcode_command_line_tools
+    set_xcode_developer_directory
+    agree_with_xcode_licence
 }
 
 main

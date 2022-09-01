6c234596962162dc6308b6cfb49f1bd9b1da8a1e
Peter Piekarczyk <peter@peterp.me>
2022-09-01 09:26:44 -0500

    adds mason

--- nvim-custom/plugins/init.lua
+++ nvim-custom/plugins/init.lua
@@ -19,4 +19,30 @@ return {
    ["nathom/filetype.nvim"] = {},
    ["nkrkv/nvim-treesitter-rescript"] = {},
    ["rescript-lang/vim-rescript"] = { ft = "rescript" },
+
+   ["williamboman/mason.nvim"] = {
+      ensure_installed = {
+        -- lua stuff
+        "lua-language-server",
+        "stylua",
+
+        -- web dev
+        "css-lsp",
+        "deno",
+        "eslint_d",
+        "graphql-language-service-cli",
+        "html-lsp",
+        "json-lsp",
+        "markdownlint",
+        "prettierd",
+        "reason-language-server",
+        "tailwindcss-language-server",
+        "typescript-language-server",
+        "yaml-language-server",
+
+        -- shell
+        "shfmt",
+        "shellcheck",
+      },
+    },
 }

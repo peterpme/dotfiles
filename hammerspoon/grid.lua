08bb6564b0733bb3c122b534ec2fd3d2bed625eb
Peter Piekarczyk <peter@peterp.me>
2022-10-31 20:22:15 -0500

    hammerspoon config

--- hammerspoon/grid.lua
+++ hammerspoon/grid.lua
@@ -1,3 +1,5 @@
+local alert = require "hs.alert"
+
 Grid = {}
 Grid.BORDER = 1
 
@@ -7,201 +9,217 @@ Grid.R_CHUNK_SHARE = 0.5
 
 Grid.PEEK_PIXELS = 70
 
-function Grid.fullscreen()
-  local win = hs.window.focusedWindow()
-  if not win then return end
+local positions = {
+	fullscreen = { x = 0.00, y = 0.00, w = 1.00, h = 1.00 },
 
-  local screenframe = Grid.screenframe(win)
-  win:setFrame(screenframe)
+	right50 = { x = 0.50, y = 0.00, w = 0.50, h = 1.00 },
+	left50 = { x = 0.00, y = 0.00, w = 0.50, h = 1.00 },
+	top50 = { x = 0.00, y = 0.00, w = 1.00, h = 0.50 },
+	bottom50 = { x = 0.00, y = 0.50, w = 1.00, h = 0.50 },
+	right33 = { x = (1 / 3) * 2, y = 0.00, x2 = 1.00, h = 1.00 },
+	left33 = { x = 0.00, y = 0.00, w = 1 / 3, h = 1.00 },
+
+	bottomCenter50 = { x = 0.25, y = 0.50, w = 0.50, h = 0.50 },
+}
+
+function alertCannotManipulateWindow()
+	alert.show("Can't move window")
 end
 
-function Grid.lefthalf()
-  local win = hs.window.focusedWindow()
-  if not win then return end
+function move(position)
+	local win = window.focusedWindow()
+	if not win then
+		alertCannotManipulateWindow()
+		return
+	end
+	win:move(position, nil, true)
+end
 
-  local screenframe = Grid.screenframe(win)
-  local newframe = {
-    x = screenframe.x,
-    y = screenframe.y,
-    w = screenframe.w / 2 - Grid.BORDER,
-    h = screenframe.h,
-  }
+function Grid.fullscreen()
+	local win = hs.window.focusedWindow()
+	if not win then
+		return
+	end
 
-  win:setFrame(newframe)
+	local screenframe = Grid.screenframe(win)
+	win:setFrame(screenframe)
 end
 
 function Grid.topHalf()
-  local win = hs.window.focusedWindow()
-  if not win then return end
+	local win = hs.window.focusedWindow()
+	if not win then
+		return
+	end
 
-  local screenframe = Grid.screenframe(win)
-  local newframe = {
-    x = screenframe.x,
-    y = screenframe.y,
-    w = screenframe.w,
-    h = screenframe.h / 2 - Grid.BORDER,
-  }
+	local screenframe = Grid.screenframe(win)
+	local newframe = {
+		x = screenframe.x,
+		y = screenframe.y,
+		w = screenframe.w,
+		h = screenframe.h / 2 - Grid.BORDER,
+	}
 
-  win:setFrame(newframe)
+	win:setFrame(newframe)
 end
 
 function Grid.bottomHalf()
-  local win = hs.window.focusedWindow()
-  if not win then return end
+	local win = hs.window.focusedWindow()
+	if not win then
+		return
+	end
 
-  local screenframe = Grid.screenframe(win)
-  local newframe = {
-    x = screenframe.x,
-    y = screenframe.y + screenframe.h / 2 + Grid.BORDER,
-    w = screenframe.w,
-    h = screenframe.h / 2 - Grid.BORDER,
-  }
+	local screenframe = Grid.screenframe(win)
+	local newframe = {
+		x = screenframe.x,
+		y = screenframe.y + screenframe.h / 2 + Grid.BORDER,
+		w = screenframe.w,
+		h = screenframe.h / 2 - Grid.BORDER,
+	}
 
-  win:setFrame(newframe)
-end
-
-function Grid.righthalf()
-  local win = hs.window.focusedWindow()
-  if not win then return end
-
-  local screenframe = Grid.screenframe(win)
-  local newframe = {
-    x = screenframe.x + screenframe.w / 2 + Grid.BORDER,
-    y = screenframe.y,
-    w = screenframe.w / 2 - Grid.BORDER,
-    h = screenframe.h,
-  }
-
-  win:setFrame(newframe)
+	win:setFrame(newframe)
 end
 
 function Grid.topleft()
-  local win = hs.window.focusedWindow()
-  if not win then return end
+	local win = hs.window.focusedWindow()
+	if not win then
+		return
+	end
 
-  local screenframe = Grid.screenframe(win)
-  local newframe = {
-    x = screenframe.x,
-    y = screenframe.y,
-    w = screenframe.w * Grid.L_CHUNK_SHARE - Grid.BORDER,
-    h = screenframe.h / 2 - Grid.BORDER,
-  }
+	local screenframe = Grid.screenframe(win)
+	local newframe = {
+		x = screenframe.x,
+		y = screenframe.y,
+		w = screenframe.w * Grid.L_CHUNK_SHARE - Grid.BORDER,
+		h = screenframe.h / 2 - Grid.BORDER,
+	}
 
-  win:setFrame(newframe)
+	win:setFrame(newframe)
 end
 
 function Grid.bottomleft()
-  local win = hs.window.focusedWindow()
-  if not win then return end
+	local win = hs.window.focusedWindow()
+	if not win then
+		return
+	end
 
-  local screenframe = Grid.screenframe(win)
-  local newframe = {
-    x = screenframe.x,
-    y = screenframe.y + screenframe.h / 2 + Grid.BORDER,
-    w = screenframe.w * Grid.L_CHUNK_SHARE - Grid.BORDER,
-    h = screenframe.h / 2 - Grid.BORDER,
-  }
+	local screenframe = Grid.screenframe(win)
+	local newframe = {
+		x = screenframe.x,
+		y = screenframe.y + screenframe.h / 2 + Grid.BORDER,
+		w = screenframe.w * Grid.L_CHUNK_SHARE - Grid.BORDER,
+		h = screenframe.h / 2 - Grid.BORDER,
+	}
 
-  win:setFrame(newframe)
+	win:setFrame(newframe)
 end
 
 function Grid.topright()
-  local win = hs.window.focusedWindow()
-  if not win then return end
+	local win = hs.window.focusedWindow()
+	if not win then
+		return
+	end
 
-  local screenframe = Grid.screenframe(win)
-  local newframe = {
-    x = screenframe.x + screenframe.w * Grid.L_CHUNK_SHARE + Grid.BORDER,
-    y = screenframe.y,
-    w = screenframe.w * Grid.R_CHUNK_SHARE - Grid.BORDER,
-    h = screenframe.h / 2 - Grid.BORDER,
-  }
+	local screenframe = Grid.screenframe(win)
+	local newframe = {
+		x = screenframe.x + screenframe.w * Grid.L_CHUNK_SHARE + Grid.BORDER,
+		y = screenframe.y,
+		w = screenframe.w * Grid.R_CHUNK_SHARE - Grid.BORDER,
+		h = screenframe.h / 2 - Grid.BORDER,
+	}
 
-  win:setFrame(newframe)
+	win:setFrame(newframe)
 end
 
 function Grid.bottomright()
-  local win = hs.window.focusedWindow()
-  if not win then return end
+	local win = hs.window.focusedWindow()
+	if not win then
+		return
+	end
 
-  local screenframe = Grid.screenframe(win)
-  local newframe = {
-    x = screenframe.x + screenframe.w * Grid.L_CHUNK_SHARE + Grid.BORDER,
-    y = screenframe.y + screenframe.h / 2 + Grid.BORDER,
-    w = screenframe.w * Grid.R_CHUNK_SHARE - Grid.BORDER,
-    h = screenframe.h / 2 - Grid.BORDER,
-  }
+	local screenframe = Grid.screenframe(win)
+	local newframe = {
+		x = screenframe.x + screenframe.w * Grid.L_CHUNK_SHARE + Grid.BORDER,
+		y = screenframe.y + screenframe.h / 2 + Grid.BORDER,
+		w = screenframe.w * Grid.R_CHUNK_SHARE - Grid.BORDER,
+		h = screenframe.h / 2 - Grid.BORDER,
+	}
 
-  win:setFrame(newframe)
+	win:setFrame(newframe)
 end
 
 function Grid.pushwindow()
-  local win = hs.window.focusedWindow()
-  if not win then return end
+	local win = hs.window.focusedWindow()
+	if not win then
+		return
+	end
 
-  local winframe = win:frame()
-  local nextscreen = win:screen():next()
-  local screenframe = nextscreen:frame()
-  local newframe = {
-    x = screenframe.x,
-    y = screenframe.y,
-    w = math.min(winframe.w, screenframe.w),
-    h = math.min(winframe.h, screenframe.h),
-  }
+	local winframe = win:frame()
+	local nextscreen = win:screen():next()
+	local screenframe = nextscreen:frame()
+	local newframe = {
+		x = screenframe.x,
+		y = screenframe.y,
+		w = math.min(winframe.w, screenframe.w),
+		h = math.min(winframe.h, screenframe.h),
+	}
 
-  win:setFrame(newframe)
+	win:setFrame(newframe)
 end
 
 function Grid.screenframe(win)
-  return win:screen():frame()
+	return win:screen():frame()
 end
-
-
 
 -- Customized versions of lefthalf() and righthalf() that make the left side slightly wider:
-function Grid.leftchunk()
-  local win = hs.window.focusedWindow()
-  if not win then return end
+function Grid.leftHalf()
+	local win = hs.window.focusedWindow()
+	if not win then
+		return
+	end
 
-  local screenframe = Grid.screenframe(win)
-  local newframe = {
-    x = screenframe.x,
-    y = screenframe.y,
-    w = screenframe.w * Grid.L_CHUNK_SHARE - Grid.BORDER,
-    h = screenframe.h,
-  }
+	local screenframe = Grid.screenframe(win)
+	local newframe = {
+		x = screenframe.x,
+		y = screenframe.y,
+		w = screenframe.w * Grid.L_CHUNK_SHARE - Grid.BORDER,
+		h = screenframe.h,
+	}
 
-  win:setFrame(newframe)
+	win:setFrame(newframe)
 end
 
-function Grid.rightchunk()
-  local win = hs.window.focusedWindow()
-  if not win then return end
+function Grid.rightHalf()
+	local win = hs.window.focusedWindow()
+	if not win then
+		return
+	end
 
-  local screenframe = Grid.screenframe(win)
-  local newframe = {
-    x = screenframe.x + screenframe.w * Grid.L_CHUNK_SHARE + Grid.BORDER,
-    y = screenframe.y,
-    w = screenframe.w * Grid.R_CHUNK_SHARE - Grid.BORDER,
-    h = screenframe.h,
-  }
+	local screenframe = Grid.screenframe(win)
+	local newframe = {
+		x = screenframe.x + screenframe.w * Grid.L_CHUNK_SHARE + Grid.BORDER,
+		y = screenframe.y,
+		w = screenframe.w * Grid.R_CHUNK_SHARE - Grid.BORDER,
+		h = screenframe.h,
+	}
 
-  win:setFrame(newframe)
+	win:setFrame(newframe)
 end
 
 function Grid.rightpeek()
-  local win = hs.window.focusedWindow()
-  if not win then return end
+	local win = hs.window.focusedWindow()
+	if not win then
+		return
+	end
 
-  local screenframe = Grid.screenframe(win)
-  local newframe = {
-    x = screenframe.x + screenframe.w - Grid.PEEK_PIXELS,
-    y = screenframe.y,
-    w = screenframe.w * Grid.R_CHUNK_SHARE - Grid.BORDER,
-    h = screenframe.h,
-  }
+	local screenframe = Grid.screenframe(win)
+	local newframe = {
+		x = screenframe.x + screenframe.w - Grid.PEEK_PIXELS,
+		y = screenframe.y,
+		w = screenframe.w * Grid.R_CHUNK_SHARE - Grid.BORDER,
+		h = screenframe.h,
+	}
 
-  win:setFrame(newframe)
+	win:setFrame(newframe)
 end
 
 return Grid
--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,83 +1,99 @@
-local application = require "hs.application"
-local hotkey = require "hs.hotkey"
-local alert = require "hs.alert"
-local Grid = require 'grid'
+local application = require("hs.application")
+local hotkey = require("hs.hotkey")
+local alert = require("hs.alert")
+local Grid = require("grid")
 
-require "wifi_control"
+require("wifi_control")
 -- require "lights"
 
 local mashGeneral = {
-  'cmd',
-  'ctrl'
+	"cmd",
+	"ctrl",
+}
+
+local mashExtra = {
+	"cmd",
+	"ctrl",
+	"shift",
 }
 
 local mashApps = {
-  'cmd',
-  'ctrl'
+	"cmd",
+	"ctrl",
 }
 
 local screenKeys = {
-  'cmd',
-  'ctrl',
-  'shift'
+	"cmd",
+	"ctrl",
+	"shift",
 }
 
 for screenIndex = 1, 3 do
-  hs.hotkey.bind(screenKeys, tostring(screenIndex), function()
-    local win = hs.window.focusedWindow()
-    local screen = hs.screen.allScreens()[screenIndex]
+	hs.hotkey.bind(screenKeys, tostring(screenIndex), function()
+		local win = hs.window.focusedWindow()
+		local screen = hs.screen.allScreens()[screenIndex]
 
-    win:moveToScreen(screen)
-    win:setFrame(screen:frame())
-  end)
+		win:moveToScreen(screen)
+		win:setFrame(screen:frame())
+	end)
 end
 
 hs.hotkey.bind(mashGeneral, "S", function()
-  for i, screen in ipairs(hs.screen.allScreens()) do
-    hs.alert("Screen " .. i, {}, screen)
-  end
+	for i, screen in ipairs(hs.screen.allScreens()) do
+		hs.alert("Screen " .. i, {}, screen)
+	end
 end)
 
 -- Disable window animations (janky for iTerm)
 hs.window.animationDuration = 0
 
 -- Hammerspoon repl
-hs.hotkey.bind(mashGeneral, 'C', hs.openConsole)
+hs.hotkey.bind(mashGeneral, "C", hs.openConsole)
 
 -- Window Management
-hs.hotkey.bind(mashGeneral, 'O', Grid.fullscreen)
-hs.hotkey.bind(mashGeneral, 'H', Grid.leftchunk)
-hs.hotkey.bind(mashGeneral, 'L', Grid.rightchunk)
-hs.hotkey.bind(mashGeneral, 'K', Grid.topHalf)
-hs.hotkey.bind(mashGeneral, 'J', Grid.bottomHalf)
+hs.hotkey.bind(mashGeneral, "O", Grid.fullscreen)
+hs.hotkey.bind(mashGeneral, "H", Grid.leftHalf)
+hs.hotkey.bind(mashGeneral, "L", Grid.rightHalf)
+hs.hotkey.bind(mashGeneral, "K", Grid.topHalf)
+hs.hotkey.bind(mashGeneral, "J", Grid.bottomHalf)
 
-hs.hotkey.bind(mashGeneral, 'U', Grid.topleft)
-hs.hotkey.bind(mashGeneral, 'N', Grid.bottomleft)
-hs.hotkey.bind(mashGeneral, 'I', Grid.topright)
-hs.hotkey.bind(mashGeneral, 'M', Grid.bottomright)
+-- Thirds
+-- hs.hotkey.bind(mashExtra, "H", Grid.leftThird)
+-- hs.hotkey.bind(mashExtra, "L", Grid.rightThird)
+
+hs.hotkey.bind(mashGeneral, "U", Grid.topleft)
+hs.hotkey.bind(mashGeneral, "N", Grid.bottomleft)
+hs.hotkey.bind(mashGeneral, "I", Grid.topright)
+hs.hotkey.bind(mashGeneral, "M", Grid.bottomright)
 
 -- Spotify
-hs.hotkey.bind(mashGeneral, 'P', hs.spotify.play)
-hs.hotkey.bind(mashGeneral, 'Y', hs.spotify.pause)
-hs.hotkey.bind(mashGeneral, 'T', hs.spotify.displayCurrentTrack)
+hs.hotkey.bind(mashGeneral, "P", hs.spotify.play)
+hs.hotkey.bind(mashGeneral, "Y", hs.spotify.pause)
+hs.hotkey.bind(mashGeneral, "T", hs.spotify.displayCurrentTrack)
 
 -- Slack-specific app launcher (since I keep it "peeked" to the side by default)
 function showSlack()
-  local appName = 'Slack'
-  local app = hs.application.find(appName)
-  hs.application.launchOrFocus(appName)
+	local appName = "Slack"
+	local app = hs.application.find(appName)
+	hs.application.launchOrFocus(appName)
 
-  if (app and hs.application.isRunning(app)) then
-    Grid.topleft()
-  end
+	if app and hs.application.isRunning(app) then
+		Grid.topleft()
+	end
 end
 
 -- App Shortcuts
-hs.hotkey.bind(mashApps, '1', function() hs.application.launchOrFocus('kitty') end)
-hs.hotkey.bind(mashApps, '2', function() hs.application.launchOrFocus('Firefox') end)
-hs.hotkey.bind(mashApps, '3', showSlack)
-hs.hotkey.bind(mashApps, '4', function() hs.application.launchOrFocus('Polymail') end)
+hs.hotkey.bind(mashApps, "1", function()
+	hs.application.launchOrFocus("kitty")
+end)
+hs.hotkey.bind(mashApps, "2", function()
+	hs.application.launchOrFocus("Firefox")
+end)
+hs.hotkey.bind(mashApps, "3", showSlack)
+hs.hotkey.bind(mashApps, "4", function()
+	hs.application.launchOrFocus("Polymail")
+end)
 
 -- Reload automatically on config changes
-hs.pathwatcher.new(os.getenv('HOME') .. '/.hammerspoon/', hs.reload):start()
-hs.alert('Hammerspoon is locked and loaded', 1)
+hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", hs.reload):start()
+hs.alert("Hammerspoon is locked and loaded", 1)

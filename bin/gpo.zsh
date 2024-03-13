66388c7a9f59bd95dd173ac60cb16bde301f6037
Peter Piekarczyk <peterpme@users.noreply.github.com>
2024-03-13 14:50:08 -0500

    adds gpo command

---
+++ bin/gpo.zsh
@@ -0,0 +1,32 @@
+}
+#!/bin/zsh
+
+# Function to commit, push, and create a new branch with the commit message
+function gpo() {
+  if [ -z "$1" ]; then
+    echo "Please provide a commit message."
+    return 1
+  fi
+
+  commit_message="$1"
+  open_web=false
+
+  # Check if the -w flag is provided
+  if [ "$2" = "-w" ]; then
+    open_web=true
+  fi
+
+  # Commit changes
+  git commit -m "$commit_message"
+
+  # Push changes to the current branch
+  gp
+
+  # Create a pull request
+  prc "$commit_message"
+
+  # Open the pull request in the web browser if the -w flag is provided
+  if $open_web; then
+    gh pr view --web
+  fi
+}

bbd557015dcff32bd3d4eb44477c38f2538da4f7
Peter Piekarczyk <peterpme@users.noreply.github.com>
2024-08-27 15:28:14 -0500

    updates

--- nvim-custom/chadrc.lua
+++ nvim-custom/chadrc.lua
@@ -7,7 +7,7 @@ M.plugins = "custom.plugins"
 local highlights = require("custom.highlights")
 
 M.ui = {
-	theme = "tokyodark",
+	theme = "chadracula",
 	transparency = false,
 	hl_override = highlights.override,
 	hl_add = highlights.add,
